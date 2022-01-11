5ffe6e1711086634c28c9fee721369580c56a1d9
mhahn <mhahn@student.42heilbronn.de>
2022-01-11 22:04:24 +0100

    Added philosophers initialization

--- philo/delegate.c
+++ philo/delegate.c
@@ -15,15 +15,34 @@ struct s_delegate	*delegate_new(size_t philo_count)
 
 void	delegate_create(struct s_delegate *this, size_t philo_count)
 {
+	size_t	i;
+
 	if (this == NULL)
 		return ;
-	// TODO
+	this->time_to_sleep = 0;
+	this->time_to_eat = 0;
+	this->time_to_die = 0;
+	this->philo_count = philo_count;
+	i = 0;
+	while (i < philo_count)
+	{
+		philo_create(this->philosophers[i]);
+		i++;
+	}
 }
 
 void	delegate_destroy(struct s_delegate *this)
 {
+	size_t	i;
+
 	if (this == NULL)
 		return ;
+	i = 0;
+	while (i < philo_count)
+	{
+		philo_destroy(this->philosophers[i]);
+		i++;
+	}
 }
 
 void	delegate_delete(struct s_delegate *this)
