ea9511520f9b60d97dd24a6d5378ba995bedea3e
mhahn <mhahn@student.42heilbronn.de>
2022-01-11 21:41:35 +0100

    Added state enum

---
+++ philo/state.h
@@ -0,0 +1,15 @@
+#ifndef STATE_H
+# define STATE_H
+
+/*
+ * An enumeration with the possible states a philosopher can have.
+ */
+enum e_state {
+	DIED,
+	EATING,
+	SLEEPING,
+	THINKING,
+	UNDEFINED
+};
+
+#endif
