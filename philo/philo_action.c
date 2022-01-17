fc69e61ec3119aa00503e044abbb6eb53dc451b0
mhahn <mhahn@student.42heilbronn.de>
2022-01-18 00:24:40 +0100

    Added death if the time to die is shorter or equal to the time to die

--- philo/philo_action.c
+++ philo/philo_action.c
@@ -8,7 +8,7 @@ enum e_state	philo_sleep_or_die(struct s_philo *this, int time)
 	struct timeval	t;
 
 	gettimeofday(&t, NULL);
-	if (time > this->delegate->time_to_die) // FIXME cannot be that inaccurate!!!
+	if (time > this->delegate->time_to_die)
 	{
 		usleep(this->delegate->time_to_die * 1000);
 		return (DIED);
@@ -23,9 +23,10 @@ enum e_state	philo_eat(struct s_philo *this, int time)
 
 	// ...
 	usleep(time * 1000);
+	// ...
 	gettimeofday(&t, NULL);
-//	if (delegate_get_time_diff(&t, &this->last_eat_time) >= this->delegate->time_to_die) // FIXME init las eat time!!!
-//		return (DIED);
+	if (delegate_get_time_diff(&t, &this->last_eat_time) >= this->delegate->time_to_die)
+		return (DIED);
 	this->last_eat_time = t;
 	return (EATING);
 }
