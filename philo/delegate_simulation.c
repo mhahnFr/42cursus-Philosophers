fdbdc8eea3c218ca749277f94cedefb74c72d2a5
mhahn <mhahn@student.42heilbronn.de>
2022-01-17 15:32:06 +0100

    Added main loop

--- philo/delegate_simulation.c
+++ philo/delegate_simulation.c
@@ -1,6 +1,25 @@
 #include <pthread.h>
+#include <unistd.h>
 
 #include "delegate.h"
+
+void	delegate_async_check(struct s_delegate *this)
+{
+	int	i;
+
+	i = 0;
+	while (i < this->philo_count)
+	{
+		if (this->philosophers[i].has_died)
+		{
+			delegate_stop_simulation(this, &this->philosophers[i]);
+			return ;
+		}
+		i++;
+	}
+	usleep(100);
+	delegate_async_check(this);
+}
 
 void	delegate_start_simulation(struct s_delegate *this)
 {
@@ -16,6 +35,7 @@ void	delegate_start_simulation(struct s_delegate *this)
 			(void *) &this->philosophers[i]);
 		i++;
 	}
+	delegate_async_check(this);
 }
 
 void	delegate_stop_simulation(
