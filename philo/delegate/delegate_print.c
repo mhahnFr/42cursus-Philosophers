51054f86877036e78d89a5c4bb7a0908341dad57
mhahnFr <83553794+mhahnFr@users.noreply.github.com>
2022-02-10 17:14:57 +0100

    Added Protected printing

---
+++ philo/delegate/delegate_print.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+#include "delegate.h"
+
+void	delegate_print(struct s_delegate *this, size_t index, char *what)
+{
+	pthread_mutex_lock(&this->print_mutex);
+	printf("%d %zu %s\n", delegate_get_time_stamp(this), index, what);
+	pthread_mutex_unlock(&this->print_mutex);
+}
